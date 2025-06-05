7e40c1f72188458ab5a6f7950b11c6017c171a42
S Agarwal <bh01488273@devcorptenant.com>
2025-06-05 10:20:54 +0530

    changes

--- src/pages/Feature.tsx
+++ src/pages/Feature.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Typography,
   Button,
@@ -13,7 +13,10 @@ import {
   Col,
   Layout,
   Menu,
-  theme
+  theme,
+  TableProps,
+  Tag,
+  Table
 } from "antd";
 
 import { useLocation, useNavigate } from "react-router-dom";
@@ -22,10 +25,25 @@ import axios from "axios";
 
 const { Title } = Typography;
 const { Header, Content, Sider } = Layout;
-const items = Array.from({ length: 4 }).map((_, index) => ({
-  key: index + 1,
-  label: `nav ${index + 1}`,
-}));
+const items = [
+  {
+    key : 1,
+    label :"Epic"
+  },{
+     key : 2,
+    label :"Story"
+  },{
+    key : 3,
+    label : "File"
+  }
+];
+
+interface File {
+  file_id: string;
+  filename: string;
+  type: string;
+  status: string;
+}
 
 const Feature: React.FC = () => {
   const location = useLocation();
@@ -41,11 +59,171 @@ const Feature: React.FC = () => {
   const [loading, setLoading] = useState(false);
   const [form] = Form.useForm();
   const [statusForm] = Form.useForm();
-
+ const [messageApi, contextHolder] = message.useMessage();
+  const [columns, setColumns] = useState<TableProps<File>['columns']>([])
+  const [tableData, setTableData] = useState<File[]>([])
   const [showEpic, setShowEpic] = useState(!data.epic_sheet);
   const [showStory, setShowStory] = useState(!!data.epic_sheet);
+  const [refresh, setRefresh] = useState(0);
 
   const initialType = showEpic ? "Epic" : showStory ? "Story" : undefined;
+
+  const actions = (status: string): string[] => {
+    switch (status.toLowerCase()) {
+      case "uploaded":
+        return ['validate'];
+      case "validated":
+        return ['validate', 'generate'];
+      default:
+        return ['view'];
+    
+    }
+  };
+  let fileList : File[];
+  const success = (message) => {
+    messageApi.open({
+      type: 'success',
+      content: message,
+    });
+  };
+  const error = (errorMessage) => {
+    messageApi.open({
+      type: 'error',
+      content: errorMessage,
+    });
+  };
+ 
+
+useEffect(()=> {
+  
+  const fetchAllFiles = async () => {
+    
+    try {
+      const res = await axios.get(`http://127.0.0.1:8000/api/widgets/files/?handle=${data.handle}`)
+      fileList = res?.data.files; 
+      console.log(fileList);
+
+      fileList?.forEach((item,index) => {
+        fileList[index]["action"] = actions(item.status);
+      })
+      setTableData(fileList)
+      setColumns([
+        {
+          title: 'File Name',
+          dataIndex: 'filename',
+          key: 'filename'
+        },
+        {
+          title: 'Type',
+          dataIndex: 'type',
+          key: 'type',
+        },
+       {
+        title : 'Status',
+        dataIndex: 'status',
+        key: 'status',
+       },
+        {
+          title: 'Action',
+          key: 'action',
+          render: (_, record) => {
+            console.log(record)
+            return (
+              <>
+                {record.action.map((action: string) => (
+                  <Button
+                    key={action}
+                    onClick={() => performFileAction(action, record.type,record.file_id)}
+                    style={{ marginRight: 8 }}
+                  >
+                    {action}
+                  </Button>
+                ))}
+              </>
+            );
+          },
+        },
+      ])
+      
+    } catch (err : any) {
+      error(err.response?.data?.error || "Failed to load files")
+    } finally {
+      setLoading(false);
+    }
+  };
+  fetchAllFiles();
+
+},[refresh]);
+
+const performFileAction = async (action : string,type :string,file_id : string)=>{
+if(action =='validate'){
+  try {
+    let res;
+    if (type === "epic") {
+        res = await axios.post(`http://127.0.0.1:8000/api/widgets/validate-epic/`, {
+        handle: data.handle,
+        file_id :file_id
+      },{responseType: "blob"});
+      
+    } else {
+      res = await axios.post(`http://127.0.0.1:8000/api/widgets/validate-story/`, {
+        handle: data.handle,
+        file_id :file_id
+      },{responseType: "blob"});
+    }
+    const url = window.URL.createObjectURL(new Blob([res.data]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'validation_result.csv'); // change filename as needed
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    success("Validated");
+  }
+    catch (err: any) {
+    error(err.response?.data?.error || `Failed to validate ${type}`);
+    }
+ 
+}else if(action == 'generate'){
+  try {
+    if (type === "epic") {
+      await axios.post(`http://127.0.0.1:8000/api/stories/generate-epics/`, {
+        handle: data.handle,
+        file_id :file_id
+      });
+    } else {
+      await axios.post(`http://127.0.0.1:8000/api/stories/generate-stories/`, {
+        handle: data.handle,
+        file_id :file_id
+      });
+    }
+
+    success(`${type.charAt(0).toUpperCase() + type.slice(1)} generated successfully`);
+  } catch (err: any) {
+    error(err.response?.data?.error || `Failed to generate ${type}`);
+  }
+  
+}else{
+  console.log('view');
+}
+
+setRefresh(prev => prev + 1);
+}
+
+// useEffect(() => {
+//   const fetchAllFiles = async () => {
+//     try {
+//       const res = await axios.get('http://127.0.0.1:8000/api/widgets/get-files/'); 
+  
+//       message.success(res.data.message);
+//     } catch (err : any) {
+//       message.error(err.response?.data?.error || "Failed to load files")
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+// fetchAllFiles();
+// }, []);
 
   const handleOpen = () => {
     setModalVisible(true);
@@ -73,9 +251,9 @@ const Feature: React.FC = () => {
         handle: data.handle,
         issue_type: type,
       });
-      message.success(`${type} panel clicked!`);
+      success(`${type} panel clicked!`);
     } catch (err: any) {
-      message.error("Failed to send panel click event");
+      error("Failed to send panel click event");
     }
   };
 
@@ -111,6 +289,7 @@ const Feature: React.FC = () => {
     } finally {
       setLoading(false);
     }
+    setRefresh(prev => prev + 1);
   };
 
   const onStatusSubmit = async (values: any) => {
@@ -156,16 +335,9 @@ const Feature: React.FC = () => {
 
   return (
     <Layout>
-      <Header style={{ display: 'flex', alignItems: 'center' }}>
+      <Header style={{ display: 'flex', alignItems: 'center', justifyContent : "space-between"}}>
         <div className="demo-logo" />
-        <Title level={2} style={{ color: '#ffffff', paddingTop: 10, paddingRight: 10, textAlign: "center" }}>Home Page</Title>
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['1']}
-          items={items}
-          style={{ flex: 1, minWidth: 0 }}
-        />
+        <Title level={2} style={{ color: '#ffffff', paddingTop: 10, paddingRight: 10, textAlign: "center" }}>{data.name}</Title>
         <Button type="primary" onClick={handleLogout}>
           Logout
         </Button>
@@ -177,14 +349,16 @@ const Feature: React.FC = () => {
           <Sider style={{ background: colorBgContainer }} width={200}>
             <Menu
               mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
+              defaultSelectedKeys={['3']}
+              defaultOpenKeys={['file']}
               style={{ height: '100%' }}
               items={items}
             />
           </Sider>
           <Content style={{ padding: '48px' }}>
             <div style={{ padding: 20, position: "relative" }}>
+              <Layout>
+              <Header style={{ display: 'flex', alignItems: 'center', justifyContent : "end-end",padding: 0, background: colorBgContainer}}>
               <Button
                 type="primary"
                 style={{ position: "absolute", top: 10, right: 100 }}
@@ -199,21 +373,26 @@ const Feature: React.FC = () => {
               >
                 Upload
               </Button>
-
-              <Title level={2} style={{ textAlign: "center" }}>{data.name}</Title>
-
+            </Header>
+            <Content>
+            {contextHolder}
+            <Table<File> columns={columns} dataSource={tableData} />
+            </Content>
+            </Layout>
+              {/* <Title level={2} style={{ textAlign: "center" }}>{data.name}</Title> */}
+{/* 
               <div style={{ display: "flex", justifyContent: "center", gap: "16px", marginBottom: "20px" }}>
                 <Button type="primary" onClick={() => handleGenerate("epic")}>Generate Epic</Button>
                 <Button type="primary" onClick={() => handleGenerate("story")}>Generate Story</Button>
-              </div>
+              </div> */}
 
-              <Row gutter={16} justify="center" style={{ marginTop: 20 }}>
+              {/* <Row gutter={16} justify="center" style={{ marginTop: 20 }}>
                 <Col span={6}><Card hoverable onClick={() => onPanelClick("Feature")}>Epic</Card></Col>
                 <Col span={6}><Card hoverable onClick={() => onPanelClick("Story")}>Story</Card></Col>
                 <Col span={6}><Card hoverable onClick={() => onPanelClick("File")}>File</Card></Col>
-              </Row>
+              </Row> */}
 
-              <Button type="primary" block onClick={handleStatusOpen} style={{ display: "block", margin: "20px auto", width: "120px" }}>Update</Button>
+              {/* <Button type="primary" block onClick={handleStatusOpen} style={{ display: "block", margin: "20px auto", width: "120px" }}>Update</Button> */}
 
               <Modal title="Upload Feature" open={modalVisible} onCancel={handleClose} footer={null} destroyOnClose>
                 <Form form={form} layout="vertical" onFinish={onFinish} initialValues={{ type: initialType }}>
